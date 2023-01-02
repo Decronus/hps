56864d593c947df051b95d738c40561be7465a1b
Decronus <zick3333@mail.ru>
2023-01-02 18:10:01 +0100

    report rew

--- src/components/ColorsWrap.vue
+++ src/components/ColorsWrap.vue
@@ -56,22 +56,11 @@ export default {
       type: Number,
       required: true,
     },
-  },
 
-  data() {
-    return {
-      colors: [
-        { color: "#48BF23", textColor: "Изумруд" },
-        { color: "#F19E38", textColor: "Цитрин" },
-        { color: "#8C1AF5", textColor: "Аметист" },
-        { color: "#52DCDE", textColor: "Сапфир" },
-        { color: "#CA2213", textColor: "Рубин" },
-        { color: "#ECEC33", textColor: "Гелиодор" },
-        { color: "#282828", textColor: "Шунгит" },
-        { color: "#A46F63", textColor: "Янтарь" },
-        { color: "#DE9E9C", textColor: "Гранат" },
-      ],
-    };
+    colors: {
+      type: Array,
+      required: true,
+    },
   },
 
   methods: {
